3be142a71892d2da0182bbfbfca8a8471a144f34
Anshor <anshorjihadi@gmail.com>
2019-11-09 18:58:01 +0700

    Menyelesaikan web portfolio versi 0.0.1

---
+++ precache-manifest.e8a140493bb241360bb30488c1b226b5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce20f521c924ec2cb3a6861f543cce1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3fb5fc5e673bbd4e0fc",
+    "url": "/static/css/main.cbe30711.chunk.css"
+  },
+  {
+    "revision": "ddf19dd5bdad775584a9",
+    "url": "/static/js/2.3c46b8e6.chunk.js"
+  },
+  {
+    "revision": "d3fb5fc5e673bbd4e0fc",
+    "url": "/static/js/main.eeec81cd.chunk.js"
+  },
+  {
+    "revision": "8b95b593a83404ce3bb5",
+    "url": "/static/js/runtime-main.c9c73c89.js"
+  },
+  {
+    "revision": "8dafeaf7502b47145b9179e603c68f59",
+    "url": "/static/media/bali-card.8dafeaf7.jpg"
+  },
+  {
+    "revision": "3860405c69e2806bf7000efc9d7a328d",
+    "url": "/static/media/man-2.3860405c.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/static/media/react-logo.47ce6e77.png"
+  },
+  {
+    "revision": "f9505dca4b23a04275ac3905217c87a2",
+    "url": "/static/media/undraw_1.f9505dca.png"
+  },
+  {
+    "revision": "111a795564264f985102be921cf870b5",
+    "url": "/static/media/undraw_2.111a7955.png"
+  },
+  {
+    "revision": "d0d399a8db53e0fd90618390e0c9eef9",
+    "url": "/static/media/undraw_3.d0d399a8.png"
+  },
+  {
+    "revision": "4e464d2c78b6fb8e8ed8ae8c29b25ac8",
+    "url": "/static/media/whimsical.4e464d2c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7265880ed7bccf82e14953ee192f0dcb.js"
+  "/precache-manifest.e8a140493bb241360bb30488c1b226b5.js"
 );
 
 self.addEventListener('message', (event) => {
